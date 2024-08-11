d95bc12f7c55fbb0c8dd8f3dba0d4e20509a7300
Neha Chauhan <nc10180203@gmail.com>
2024-08-12 01:27:49 +0530

    changes

--- src/components/landingPage/Navbar.jsx
+++ src/components/landingPage/Navbar.jsx
@@ -1,15 +1,16 @@
-import React from 'react';
+import Link from "next/link";
+import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className='flex justify-between w-[80%] m-auto p-1'>
-      <img src='/appLogo.png' />
-      <div className='flex items-center justify-between gap-4 font-montserrat text-[#212121] font-semibold'>
-        <span>Home</span>
-        <span>Product</span>
-        <span>Pricing</span>
-        <span>Contact</span>
-        <button className='bg-[#23A6F0] p-5 rounded-md text-white font-bold'>
+    <nav className="flex justify-between w-[80%] m-auto p-1">
+      <img src="/appLogo.png" />
+      <div className="flex items-center justify-between gap-4 font-montserrat text-[#212121] font-semibold">
+        <Link href={"/homepage"}>Home</Link>
+        <Link href={"/homepage"}>Product</Link>
+        <Link href={"/homepage"}>Pricing</Link>
+        <Link href={"/homepage"}>Contact</Link>
+        <button className="bg-[#23A6F0] p-5 rounded-xl text-white font-bold">
           Enquire now
         </button>
       </div>
